6dc399e0acc41597896b46f3df3c3c5440559ef8
datdev <Dotiendat1751998@gmail.com>
2026-01-07 13:38:29 +0700

    Initial commit

---
+++ src/main/java/com/datdevops/pgp/PgpApplication.java
@@ -0,0 +1,13 @@
+package com.datdevops.pgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datdevops/pgp/PgpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datdevops.pgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
